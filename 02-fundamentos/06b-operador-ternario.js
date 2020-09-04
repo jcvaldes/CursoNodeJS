33d40868e438642b7a1c8abc976d4a3c116276f7
Juan Carlos Valdés <idevkingos@gmail.com>
2020-09-03 21:30:03 -0300

    arquitectura base

--- 02-fundamentos/06b-operador-ternario.js
+++ 02-fundamentos/06b-operador-ternario.js
@@ -3,20 +3,19 @@
 const activo = true;
 
 // let mensaje = '';
-
+// Manera fea
 // if ( !activo ) {
 //     mensaje = 'Activo';
 // } else {
 //     mensaje = 'Inactivo';
 // }
-const mensaje = ( activo ) ? 'Activo' : 'Inactivo'; 
-//_______
- 
-console.log(mensaje)
+// const mensaje = ( activo ) ? 'Activo' : 'Inactivo'; 
 
+// Muchas veces hacemos esto con operador ternario
+// const mensaje = ( activo ) ? 'Activo' : null; 
 
+// Manera correcta de hacer lo anterior
 const mensaje = activo && 'Activo';
-
 
 
 // pegar en la consola del navegador
