f0e616e7d52a8467b78171cbc6dbcde079db5dbf
Juan Carlos Valdes <idevkingos@gmail.com>
2021-01-01 13:42:24 -0300

    Ejercicio de recursividad

--- 02-fundamentos/40-recursividad.js
+++ 02-fundamentos/40-recursividad.js
@@ -12,15 +12,12 @@ function cuenta_regresiva(numero) {
 
 cuenta_regresiva(5)
 
-
 function factorial(num) {
-  if (num < 0) 
-        return -1;
-  else if (num == 0) 
-      return 1;
+  if (num < 0) return -1
+  else if (num == 0) return 1
   else {
-      return (num * factorial(num - 1));
+    return num * factorial(num - 1)
   }
 }
 
-console.log('Factorial de 4 es 4.3.2.1 = ',  factorial(4))
+console.log('Factorial de 4 es 4.3.2.1 = ', factorial(4))
